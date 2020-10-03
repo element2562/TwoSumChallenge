32ffa1e59b4dd8aedc7bf8f170bb7db0217b83a3
element2562 <element25668@gmail.com>
2020-10-03 10:47:18 -0500

    first and final commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 3, 6, 2, 9, 4, 10, -2 };
+            Console.WriteLine(TwoSum(numbers, 6));
+        }
+        static bool TwoSum(int[] numbers, int target)
+        {
+            bool sumIsTarget = false;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                for(int j = i + 1; j < numbers.Length; j++)
+                {
+                    if(numbers[i] + numbers[j] == target)
+                    {
+                        sumIsTarget = true;
+                    }
+                }
+            }
+            return sumIsTarget;
+        }
+    }
+}
